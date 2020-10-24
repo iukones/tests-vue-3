a284097aa2458893343c6bd6daf04228bec0e871
iukones <iukones1@gmail.com>
2020-10-23 23:31:47 -0500

    initial commit project

---
+++ src/router/index.js
@@ -0,0 +1,27 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const routes = [
+  {
+    path: '/episodes',
+    alias: '/',
+    name: 'Episodes',
+    component: () => import(/* webpackChunkName: "episodes" */ '../views/Episodes.vue')
+  },
+  {
+    path: '/locations',
+    name: 'Locations',
+    component: () => import(/* webpackChunkName: "locations" */ '../views/Locations.vue')
+  },
+  {
+    path: '/characters',
+    name: 'Characters',
+    component: () => import(/* webpackChunkName: "characters" */ '../views/Characters.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
